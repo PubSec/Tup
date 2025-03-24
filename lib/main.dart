93dde962e500ca2d5648a5e1b95752fab5afbd11
PubSec <pubsecs@proton.me>
2025-03-24 13:17:38 +0300

    setup: added needed dependencies and some test code

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeView());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: HomeView(),
+    );
   }
 }
---
+++ lib/views/result_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ResultView extends StatelessWidget {
+  final String text;
+
+  const ResultView({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(title: const Text('Result'), centerTitle: true),
+    body: Container(
+      padding: const EdgeInsets.all(30.0),
+      child: Text(text, style: TextStyle(fontSize: 20)),
+    ),
+  );
+}
